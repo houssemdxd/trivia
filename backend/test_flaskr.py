20370c64460c667cb6aff369dcfbb1be32c8d4d5
houssemdxd <gameplayclip4@gmail.com>
2022-08-27 23:57:54 +0100

    Add files via upload

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -33,6 +33,58 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_by_categories(self):
+     res = self.client().get('/categories')
+     data = json.loads(res.data)
+     self.assertEqual(res.status_code, 200)
+     self.assertEqual(data["success"], True)
+     self.assertTrue(data["categories"])
+     
+    def verif_delete_question(self):
+        res = self.client().delete('/questions/1')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True) 
+        
+    def verif_post_question(self):
+        res = {'question': 'why you cry',
+            'answer': 'im afraid',
+            'difficulty': 4,'category': 1}
+        res = self.client().post('/questions', json=res)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+   
+    def verif_do_search(self):
+        search = {'searchTerm': 'hou', }
+        res = self.client().post('/search', json=search)
+        data = json.loads(res.data)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(res.status_code, 200)
+        
+    def verif__questions_in_category(self):
+        res = self.client().get('/categories/2/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['current_category'], 'horror')
+    
+    def test_quiz(self):
+        quiz = {
+            'previous_questions': [1],
+            'quiz_category': {
+                'type': 'a',
+                'id': '1'
+            }
+        }
+        res = self.client().post('/quizzes', json=quiz)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        
+        
+    
+        
 
 
 # Make the tests conveniently executable
